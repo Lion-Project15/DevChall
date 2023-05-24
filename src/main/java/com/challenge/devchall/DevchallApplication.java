6735cca6bf9307d86797dd96d40ab3de8327b93e
SeoMoonk <sunghoon1216@naver.com>
2023-05-24 16:11:41 +0900

    Init: Settings  프로젝트 세

---
+++ src/main/java/com/challenge/devchall/DevchallApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.devchall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevchallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevchallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/devchall/DevchallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.devchall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevchallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
