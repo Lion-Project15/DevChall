038a46b893273a3f91c272959504e629ed826c5e
leemimi <mijeong1015@naver.com>
2023-05-25 16:22:54 +0900

    Feat: Member entity 엔티티 생성

--- src/main/java/com/challenge/devchall/member/entity/Member.java
+++ src/main/java/com/challenge/devchall/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.challenge.devchall.member.entity;
 
 import com.challenge.devchall.base.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,20 @@ import lombok.experimental.SuperBuilder;
 @Getter
 public class Member extends BaseEntity {
 
+    @Column(unique = true)
+    private Long loginID; //회원 로그인 id
+    private String username; //본명
+    private String password;
+
+    @Column(unique = true)
+    private String nickname;
+
+    @Column(unique = true)
+    private String  email;
+
+    private String emailToken; //이메일 토큰
+    private boolean isValid; // 인증 여부
+
+    //role은 spring security 이후에 작성
 
 }
