f0f147c77fc13699f1d93f5898fc150e62efbe20
Minizi <64017307+leemimi@users.noreply.github.com>
2023-05-26 11:18:59 +0900

    Feat: Create Login html (#13)
    * Feat: Create Login html
    로그인 html 작성 후 member변수와 연동
    
    * Docs: .gitignore add application-secret.yml

--- src/main/java/com/challenge/devchall/member/controller/MemberController.java
+++ src/main/java/com/challenge/devchall/member/controller/MemberController.java
@@ -1,4 +1,30 @@
 package com.challenge.devchall.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/usr/member")
 public class MemberController {
+
+    @GetMapping("/login")
+    public String showLogin () {
+        return "/usr/member/login";
+    }
+
+    @PostMapping("/login")
+    public String showLogin(
+            @RequestParam String loginID,
+            @RequestParam String password
+    ) {
+        System.out.println("loginID = " + loginID);
+        System.out.println("password = " + password);
+
+        return "redirect:/";
+    }
 }
