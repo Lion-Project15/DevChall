3186c30a7467a3bf48aeac25e086bfe2230c7802
Y39 <rla4878@gmail.com>
2023-05-31 15:38:18 +0900

    Feat: Apply layout to Main Page
    The Main Page theme is DevChall

---
+++ src/main/java/com/challenge/devchall/main/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.challenge.devchall.main.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping("/")
+    public String showMain(){
+        return "index";
+    }
+}
