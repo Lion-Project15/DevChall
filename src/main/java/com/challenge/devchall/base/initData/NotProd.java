f8c46681be66d1cfbb212e3f022ab62b7b567eac
SeoMoonk <sunghoon1216@naver.com>
2023-05-31 15:02:34 +0900

    Feat: Connection ChallengeMember 챌린지 생성, 참여시에 챌린지 멤버 연결

--- src/main/java/com/challenge/devchall/base/initData/NotProd.java
+++ src/main/java/com/challenge/devchall/base/initData/NotProd.java
@@ -2,6 +2,7 @@ package com.challenge.devchall.base.initData;
 
 import com.challenge.devchall.challange.service.ChallengeService;
 import com.challenge.devchall.challengepost.service.ChallengePostService;
+import com.challenge.devchall.member.entity.Member;
 import com.challenge.devchall.member.service.MemberService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -18,18 +19,18 @@ public class NotProd {
             ChallengePostService challengePostService
     ) {
         return args -> {
-            memberService.join("admin","1234", "admin@admin.com", "관리자", "관리자"); //admin 계정
-            memberService.join("user1", "1234","user1@devchall.com", "user1", "user1");
-            memberService.join("user2", "1234","user2@devchall.com", "user2", "user2");
-            memberService.join("user3", "1234","user3@devchall.com","user3","user3");
-            memberService.join("user4", "1234", "user4@devchall.com","user4","user4");
+            Member admin = memberService.join("admin", "1234", "admin@admin.com", "관리자", "관리자");//admin 계정
+            Member user1 = memberService.join("user1", "1234", "user1@devchall.com", "user1", "user1");
+            Member user2 = memberService.join("user2", "1234", "user2@devchall.com", "user2", "user2");
+            Member user3 = memberService.join("user3", "1234", "user3@devchall.com", "user3", "user3");
+            Member user4 = memberService.join("user4", "1234", "user4@devchall.com", "user4", "user4");
 
             challengeService.createChallenge("1번 챌린지", "1번 챌린지 내용입니다", "public", "day1",
-                    "2023-06-01", "2023-06-30");
+                    "2023-06-01", "2023-06-30", admin);
             challengeService.createChallenge("2번 챌린지", "2번 챌린지 내용입니다", "private", "day3",
-                    "2023-06-01", "2023-06-30");
+                    "2023-06-01", "2023-06-30", user1);
             challengeService.createChallenge("3번 챌린지", "3번 챌린지 내용입니다", "public", "day7",
-                    "2023-06-01", "2023-06-30");
+                    "2023-06-01", "2023-06-30", user2);
 
             challengePostService.write("1-1인증", "1-1인증 내용입니다.", "public", 1);
             challengePostService.write("1-2인증", "1-2인증 내용입니다.", "public", 1);
--- src/main/java/com/challenge/devchall/challange/controller/ChallengeController.java
+++ src/main/java/com/challenge/devchall/challange/controller/ChallengeController.java
@@ -4,14 +4,20 @@ package com.challenge.devchall.challange.controller;
 import com.challenge.devchall.challange.entity.Challenge;
 import com.challenge.devchall.challange.repository.ChallengeRepository;
 import com.challenge.devchall.challange.service.ChallengeService;
+import com.challenge.devchall.challengeMember.entity.ChallengeMember;
+import com.challenge.devchall.challengeMember.repository.ChallengeMemberRepository;
 import com.challenge.devchall.challengepost.entity.ChallengePost;
+import com.challenge.devchall.member.entity.Member;
+import com.challenge.devchall.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,6 +26,8 @@ public class ChallengeController {
 
     private final ChallengeService challengeService;
     private final ChallengeRepository challengeRepository;
+    private final MemberRepository memberRepository;
+    private final ChallengeMemberRepository challengeMemberRepository;
 
 
     @PreAuthorize("isAuthenticated()")
@@ -38,10 +46,13 @@ public class ChallengeController {
             @RequestParam String status,
             @RequestParam String frequency,
             @RequestParam String startDate,
-            @RequestParam String endDate
+            @RequestParam String endDate,
+            Principal principal
     ){
 
-        challengeService.createChallenge(title, contents, status, frequency, startDate, endDate);
+        Member loginMember = memberRepository.findByLoginID(principal.getName()).orElse(null);
+
+        challengeService.createChallenge(title, contents, status, frequency, startDate, endDate, loginMember);
 
         return "redirect:/";
     }
@@ -56,11 +67,16 @@ public class ChallengeController {
     }
 
     @GetMapping("/detail/{id}")
-    public String showDetail(Model model, @PathVariable("id") long id){
+    public String showDetail(Model model, @PathVariable("id") long id, Principal principal){
+
+        String name = principal.getName();
+        Member member = memberRepository.findByUsername(name).orElse(null);
 
         Challenge challenge = this.challengeService.getChallengeById(id);
         boolean hasPost = challengeService.hasPost(challenge);
-        
+
+        Optional<ChallengeMember> byChallenger = challengeMemberRepository.findByChallenger(member);
+
         if(hasPost){
             List<ChallengePost> challengePostList = challenge.getChallengePostList();
 
