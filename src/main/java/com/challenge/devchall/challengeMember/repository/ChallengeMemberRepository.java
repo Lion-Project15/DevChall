ecaa5b92b571cb48d00d0b154236f3b1efa29ba2
Y39 <rla4878@gmail.com>
2023-06-01 18:10:08 +0900

    Feat: Apply dynamic  my-challege on main page
    Show my challenge on main page

--- src/main/java/com/challenge/devchall/challengeMember/repository/ChallengeMemberRepository.java
+++ src/main/java/com/challenge/devchall/challengeMember/repository/ChallengeMemberRepository.java
@@ -5,11 +5,12 @@ import com.challenge.devchall.challengeMember.entity.ChallengeMember;
 import com.challenge.devchall.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChallengeMemberRepository extends JpaRepository<ChallengeMember, Long> {
 
-    Optional<ChallengeMember> findByChallenger(Member member);
+    List<ChallengeMember> findByChallenger(Member member);
 
     Optional<ChallengeMember> findByLinkedChallenge(Challenge challenge);
 
--- src/main/java/com/challenge/devchall/main/controller/MainController.java
+++ src/main/java/com/challenge/devchall/main/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.challenge.devchall.main.controller;
 
+import com.challenge.devchall.base.rq.Rq;
 import com.challenge.devchall.challange.service.ChallengeService;
+import com.challenge.devchall.challengeMember.service.ChallengeMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MainController {
     final private ChallengeService challengeService;
+    final private ChallengeMemberService challengeMemberService;
+    private final Rq rq;
     @GetMapping("/")
     public String showMain(Model model){
+        if(rq.isLogin()){
+            model.addAttribute("challengeMembers"
+                    , challengeMemberService.getByMember(rq.getMember()));
+        }
         model.addAttribute("challenges",challengeService.getChallengList());
         return "index";
     }
