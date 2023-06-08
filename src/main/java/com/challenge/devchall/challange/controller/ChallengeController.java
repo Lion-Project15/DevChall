828518e98528b506927604748874b7a482f7df60
SeoMoonk <sunghoon1216@naver.com>
2023-06-08 14:46:40 +0900

    Fix: MultipartFile is only available image 첨부파일은 이미지만 가능(이미지가 아니면 생성 x)

--- src/main/java/com/challenge/devchall/challange/controller/ChallengeController.java
+++ src/main/java/com/challenge/devchall/challange/controller/ChallengeController.java
@@ -1,6 +1,7 @@
 package com.challenge.devchall.challange.controller;
 
 
+import com.challenge.devchall.base.rsData.RsData;
 import com.challenge.devchall.challange.entity.Challenge;
 import com.challenge.devchall.challange.repository.ChallengeRepository;
 import com.challenge.devchall.challange.service.ChallengeService;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -38,12 +40,13 @@ public class ChallengeController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
-    public String createChallenge(){
+    public String createChallenge() {
 
         System.out.println("get mapping");
 
         return "/usr/challenge/create_form";
     }
+
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
     public String createChallenge(
@@ -55,6 +58,17 @@ public class ChallengeController {
             Principal principal
     ) throws IOException {
 
+        RsData<String> imgRsData = isImgFile(file.getOriginalFilename());
+
+        if (!file.isEmpty() && imgRsData.isSuccess()) {
+            System.out.println(imgRsData.getMsg());
+        }else{
+            //toast ui warning으로 처리?
+            System.out.println(imgRsData.getMsg());
+            return "redirect:/";
+        }
+
+
         String photoUrl = photoService.photoUpload(file);
 
         Member loginMember = memberService.getByLoginId(principal.getName());
@@ -62,12 +76,11 @@ public class ChallengeController {
         challengeService.createChallenge(title, contents, status, frequency, startDate, period,
                 language, subject, posttype, photoUrl, loginMember);
 
-        //챌린지가 img 소스를 가지도록
-
         return "redirect:/";
     }
+
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
 
         List<Challenge> challengeList = this.challengeRepository.findAll();
         model.addAttribute("challengeList", challengeList);
@@ -77,24 +90,24 @@ public class ChallengeController {
     }
 
     @GetMapping("/detail/{id}")
-    public String showDetail(Model model, @PathVariable("id") long id, Principal principal){
+    public String showDetail(Model model, @PathVariable("id") long id, Principal principal) {
 
         Challenge challenge = this.challengeService.getChallengeById(id);
-        Member loginMember= memberService.getByLoginId(principal.getName());
+        Member loginMember = memberService.getByLoginId(principal.getName());
 
         Optional<ChallengeMember> byChallengeAndMember = challengeMemberService.getByChallengeAndMember(challenge, loginMember);
 
         boolean isJoin;
 
-        if(byChallengeAndMember.isPresent()){
+        if (byChallengeAndMember.isPresent()) {
             isJoin = true;
-        }else{
+        } else {
             isJoin = false;
         }
 
         boolean hasPost = challengeService.hasPost(challenge);
 
-        if(hasPost){
+        if (hasPost) {
             List<ChallengePost> challengePostList = challenge.getChallengePostList();
 
             model.addAttribute("challengePostList", challengePostList);
@@ -105,6 +118,21 @@ public class ChallengeController {
         model.addAttribute("isJoin", isJoin);
 
         return "/usr/challenge/detail";
+    }
+
+    public RsData<String> isImgFile(String fileName) {
+
+        //확장자 추출
+        String fileExtension = StringUtils.getFilenameExtension(fileName);
+
+        if (fileExtension != null && (fileExtension.equals("jpg") || fileExtension.equals("jpeg")
+                || fileExtension.equals("png") || fileExtension.equals("gif"))){
+
+            return RsData.of("S-6", "이미지가 맞습니다.");
+        }
+        else
+            return RsData.of("F-6", "이미지만 업로드가 가능합니다.");
+
     }
 
 }
