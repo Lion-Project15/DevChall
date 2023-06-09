982374216181d863f5af00c647fba88a814261d2
Minizi <64017307+leemimi@users.noreply.github.com>
2023-06-09 14:46:14 +0900

    소셜 로그인 도입 (네이버,카카오) (#101)
    * Feat: oauth2 Kakao&Naver Add
    
    * Feat: NAVER,KAKAO 회원가입 완료
    
    * Docs: NotProd MJ Data init
    
    * Refactor: NotProd오류 수정, nickname중복허용
    
    * Refactor: 소셜로그인 아이디 리팩토링
    
    * Refactor: Login 유효성 도입 & nav bar 오류 수정

--- src/main/java/com/challenge/devchall/DevchallApplication.java
+++ src/main/java/com/challenge/devchall/DevchallApplication.java
@@ -2,12 +2,15 @@ package com.challenge.devchall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableScheduling
+@EnableCaching
+@EnableJpaAuditing
 public class DevchallApplication {
 
 	public static void main(String[] args) {
