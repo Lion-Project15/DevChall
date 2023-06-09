ffaa30667e4286150209e4ca9c6fbf61ce8029ad
Cualone <june568@naver.com>
2023-06-09 14:44:12 +0900

    Feat : Post Title & Contents Text Limit
    인증글 작성 시 제목 25자, 내용 500자 제한
    포스트 인증글, 목록 평가 점수 표시

--- src/main/java/com/challenge/devchall/challengepost/service/ChallengePostService.java
+++ src/main/java/com/challenge/devchall/challengepost/service/ChallengePostService.java
@@ -30,8 +30,53 @@ public class ChallengePostService {
     private final ChallengeMemberRepository challengeMemberRepository;
     private final PhotoService photoService;
 
-    public ChallengePost write(String title, String contents, boolean status, long postScore, long id,
-                               String photoUrl, Member member) {
+//    public ChallengePost write(String title, String contents, boolean status, long postScore, long id,
+//                               String photoUrl, Member member) {
+//
+//        Challenge linkedChallenge = challengeService.getChallengeById(id);
+//
+//        ChallengeMember challengeMember = challengeMemberService.getByChallengeAndMember(linkedChallenge, member).orElse(null);
+//
+//        RsData<ChallengeMember> postLimitRsData = challengeMember.updatePostLimit();
+//
+//        if(postLimitRsData.isFail()){
+//            System.out.println(postLimitRsData.getMsg());
+//            return null;
+//        }
+//
+//        // 제목 길이 제한
+//        int titleMaxLength = 25;
+//        if (title.length() > titleMaxLength) {
+//            RsData.of("F-1", "제목은 25자 미만으로 작성해 주세요");
+//        }
+//
+//        // 내용 길이 제한
+//        int contentsMaxLength = 500;
+//        if (contents.length() > contentsMaxLength) {
+//            RsData.of("F-1", "내용은 500자 미만으로 작성해 주세요");
+//        }
+//
+//        String largePhoto = photoService.getLargePhoto(photoUrl);
+//        String smallPhoto = photoService.getSmallPhoto(photoUrl);
+//
+//        ChallengePost challengePost = ChallengePost.builder()
+//                .postTitle(title)
+//                .postContents(contents)
+//                .postIsPublic(status)
+//                .postScore(postScore)
+//                .linkedChallenge(linkedChallenge)
+//                .challenger(member)
+//                .largePhoto(largePhoto)
+//                .smallPhoto(smallPhoto)
+//                .build();
+//
+//        challengePostRepository.save(challengePost);
+//
+//        return challengePost;
+//    }
+
+    public RsData<ChallengePost> write(String title, String contents, boolean status, long postScore, long id,
+                                       String photoUrl, Member member) {
 
         Challenge linkedChallenge = challengeService.getChallengeById(id);
 
@@ -39,9 +84,21 @@ public class ChallengePostService {
 
         RsData<ChallengeMember> postLimitRsData = challengeMember.updatePostLimit();
 
-        if(postLimitRsData.isFail()){
+        if (postLimitRsData.isFail()) {
             System.out.println(postLimitRsData.getMsg());
             return null;
+        }
+
+        // 제목 길이 제한
+        int titleMaxLength = 25;
+        if (title.length() > titleMaxLength) {
+            return RsData.of("F-1", "제목은 최대 " + titleMaxLength + "자까지 입력할 수 있습니다.");
+        }
+
+        // 내용 길이 제한
+        int contentsMaxLength = 500;
+        if (contents.length() > contentsMaxLength) {
+            return RsData.of("F-1", "내용은 최대 " + contentsMaxLength + "자까지 입력할 수 있습니다.");
         }
 
         String largePhoto = photoService.getLargePhoto(photoUrl);
@@ -60,7 +117,7 @@ public class ChallengePostService {
 
         challengePostRepository.save(challengePost);
 
-        return challengePost;
+        return RsData.successOf(challengePost);
     }
 
     public ChallengePost getChallengePostById(long id){
