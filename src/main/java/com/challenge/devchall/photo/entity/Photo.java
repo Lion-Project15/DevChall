53215bec815f89b55c91bc21672917a19083cab0
SeoMoonk <sunghoon1216@naver.com>
2023-06-14 16:38:43 +0900

    Fix: local 이미지 처리 - http, https https와 http에 따라 다른 stringbuild

--- src/main/java/com/challenge/devchall/photo/entity/Photo.java
+++ src/main/java/com/challenge/devchall/photo/entity/Photo.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @ToString
 @SuperBuilder
@@ -18,37 +21,7 @@ public class Photo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Getter
     private String photoUrl;
-
-    public String getLargePhoto(){
-
-        String photoUrl = this.photoUrl;
-
-        String[] split = photoUrl.split("devchall/");
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("https://iztyfajjvmsf17707682.cdn.ntruss.com/");
-        sb.append(split[1]);
-        sb.append("?type=m&w=700&h=400&quality=90&bgcolor=FFFFFF&extopt=3");
-
-        return sb.toString();
-    }
-
-    public String getSmallPhoto(){
-
-        String photoUrl = this.photoUrl;
-
-        String[] split = photoUrl.split("devchall/");
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("https://iztyfajjvmsf17707682.cdn.ntruss.com/");
-        sb.append(split[1]);
-        sb.append("?type=m&w=200&h=115&quality=90&bgcolor=FFFFFF&extopt=0&anilimit=1");
-
-        return sb.toString();
-    }
-
 
 }
