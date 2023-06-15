65907e53d18ef4191f9d1a7e91063cae2adc9036
Y39 <102135011+39-Y@users.noreply.github.com>
2023-06-16 03:13:26 +0900

    Bug/186 (#189)
    * Fix: character 아이템 추가 및 마이페이지 수정
    
    * Fix: Character items 가격 변동
    
    ---------
    
    Co-authored-by: Y39 <rla4878@gmail.com>

--- src/main/java/com/challenge/devchall/base/initData/ItemData.java
+++ src/main/java/com/challenge/devchall/base/initData/ItemData.java
@@ -26,12 +26,14 @@ public class ItemData {
                 itemService.create("1144FC","font","1144FC", "FFFFFF",150);
                 itemService.create("3CB24F","font","3CB24F", "FFFFFF",150);
 
-                itemService.create("cow.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/cow.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 0);
-                itemService.create("lion.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/lion.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 100);
-                itemService.create("giraffe.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/giraffe.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 0);
-                itemService.create("robot.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/robot.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 0);
-                itemService.create("ghost.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/ghost.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 0);
-                itemService.create("octopus.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/octopus.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 0);
+                itemService.create("octopus.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/octopus.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 300);
+                itemService.create("robot.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/robot.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 300);
+                itemService.create("ghost.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/ghost.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 400);
+                itemService.create("cow.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/cow.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 500);
+                itemService.create("lion.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/lion.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 600);
+                itemService.create("giraffe.png","character","http://iztyfajjvmsf17707682.cdn.ntruss.com/store_img/giraffe.png?type=m&w=120&h=120&bgcolor=EBEBEB", " ", 700);
+
+
 
             }
         };
